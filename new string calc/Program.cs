7d8dc2525435631d6e579c26dc2b77d370ff7d27
Melih KVNO <melihesad@hotmail.de>
2023-09-12 17:50:21 +0200

    testing with new branches

--- new string calc/Program.cs
+++ new string calc/Program.cs
@@ -11,17 +11,19 @@ internal class Program
             //-------------------------
 
             // String Array damit es im .Split die Vorgabe erfüllt eines string[], ansonsten wäre natürlich char[] besser.
-            string[] ops = { "+", "-", "*", "/","%"};
-            string[] numf = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0",",",".", " " };
+            string[] ops = { "+", "-", "*", "/", "%"};
+            string[] numf = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", ".", " "};
             //string[] chars = { "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" }; // hatte keine lust
 
-            
+
             //-------------------------
             //Variablen
             //-------------------------
             Console.WriteLine("Bitte die Formel mit 2 Zahlen eingeben");
             string input = Console.ReadLine();
             bool fail = false;
+            int counter = 0;
+            double res = 0;
 
             //-------------------------
             //Fehlersuche
@@ -48,50 +50,111 @@ internal class Program
             //storage Arrays zum weiternutzen
             //-------------------------------
 
-            string[] numstorage = input.Split(ops, StringSplitOptions.RemoveEmptyEntries);
-            string[] opstorage = input.Split(numf, StringSplitOptions.RemoveEmptyEntries);
+            string[] tempNumStorage = input.Split(ops, StringSplitOptions.RemoveEmptyEntries);
+            string[] tempOpStorage = input.Split(numf, StringSplitOptions.RemoveEmptyEntries);
+
+            
 
             //Fehler bei zu wenigen Eingaben.
-            
-            if (numstorage.Length < 2 || numstorage.Length > 2)
+
+            if (tempNumStorage.Length < 2)
             {
                 Console.WriteLine("Fehler: Du hast nur eine oder zu viele Zahlen eingegeben.");
                 continue;
             }
 
-            //-------------------------
-            //Variablen zum Rechnen
-            //-------------------------
-            
+            List<double> numStorage = new List<double>(); //Erstellung von einer List um Werte besser zu nutzen
+            List<string> opStorage = new List<string>();
 
-            double n1 = Convert.ToDouble(numstorage[0]);
-            double n2 = Convert.ToDouble(numstorage[1]);
-            double res = 0;
-
-            //-------------------------
-            //Conditional Calculating
-            //-------------------------
-
-            switch (opstorage[0])
+            foreach (string num in tempNumStorage)
             {
-                case "+":
-                    res = n1 + n2;
-                    break;
-                case "-":
-                    res = n1 - n2;
-                    break;
-                case "*":
-                    res = n1 * n2;
-                    break;
-                case "/":
-                    res = n1 / n2;
-                    break;
-                case "%":
-                    res = n1 % n2;
-                    break;
-                default:
-                    break;
+                numStorage.Add(Convert.ToDouble(num));
             }
+
+            foreach (string op in tempOpStorage)
+            {
+                opStorage.Add(op);
+            }
+
+
+            foreach (string op in opStorage)
+            {
+                if (op == "*")
+                {
+                    res = numStorage[counter] * numStorage[++counter];
+                    numStorage.RemoveRange(counter, counter++);
+                    numStorage.Insert(counter, res);
+                    opStorage.RemoveRange(counter, counter);
+                    //counter++;
+                }
+                else if (op == "/")
+                {
+                    res = numStorage[counter] / numStorage[counter + 1];
+                    numStorage.RemoveRange(counter, counter + 1);
+                    numStorage.Insert(counter, res);
+                    opStorage.RemoveRange(counter, counter);
+                    //counter++;
+                }
+                else if (op == "=") { counter = 0; break; }
+                    counter++;
+                
+            }
+
+            foreach (string op in opStorage)
+            {
+                if (op == "+")
+                {
+                    res = numStorage[counter] + numStorage[counter + 1];
+                    numStorage.RemoveRange(counter, counter + 1);
+                    numStorage.Insert(counter, res);
+                    counter++;
+                }
+                else if (op == "-")
+                {
+                    res = numStorage[counter] / numStorage[counter + 1];
+                    numStorage.RemoveRange(counter, counter + 1);
+                    numStorage.Insert(counter, res);
+                    counter++;
+                }
+                else if (op == "=") { break; }
+                else
+                {
+                    counter++;
+                }
+            }
+
+            ////-------------------------
+            ////Variablen zum Rechnen
+            ////-------------------------
+
+
+            //double n1 = Convert.ToDouble(tempNumStorage[0]);
+            //double n2 = Convert.ToDouble(tempNumStorage[1]);
+
+            ////-------------------------
+            ////Conditional Calculating
+            ////-------------------------
+
+            //switch (opstorage[0])
+            //{
+            //    case "+":
+            //        res = n1 + n2;
+            //        break;
+            //    case "-":
+            //        res = n1 - n2;
+            //        break;
+            //    case "*":
+            //        res = n1 * n2;
+            //        break;
+            //    case "/":
+            //        res = n1 / n2;
+            //        break;
+            //    case "%":
+            //        res = n1 % n2;
+            //        break;
+            //    default:
+            //        break;
+            //}
 
             //-------------------------
             //Ergebnis
